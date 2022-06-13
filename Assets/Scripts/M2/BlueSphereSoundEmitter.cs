6ff3f37b40ebc7bcdb83629aa65863c06c8bd156
Aish <aish@aishraj.com>
2022-06-12 21:56:02 -0700

    Fix sound

--- Assets/Scripts/M2/BlueSphereSoundEmitter.cs
+++ Assets/Scripts/M2/BlueSphereSoundEmitter.cs
@@ -10,7 +10,7 @@ public class BlueSphereSoundEmitter : MonoBehaviour
         if (c.impulse.magnitude > 0.05f)
         {
             //we'll just use the first contact point for simplicity
-            EventManager.TriggerEvent<BoxCollisionEvent, Vector3, float>(c.contacts[0].point, c.impulse.magnitude);
+            EventManager.TriggerEvent<BombBounceEvent, Vector3>(c.contacts[0].point);
         }
             
 
